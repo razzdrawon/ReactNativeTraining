abaf1d71d28fac827aae8d015a2c75aa6ff95c3c
Ricardo Axel Padilla Gonzalez <Ricardo.Padilla@walmart.com>
2018-10-01 16:25:37 -0500

    initial state and reducer action add film

--- store/middleware.js
+++ store/middleware.js
@@ -1,3 +1,8 @@
-const fetchFilmsMiddleware = ({dispatch, getState}) => next => action => next(action);
+const fetchFilmsMiddleware = ({dispatch, getState}) => next => action => {
+
+    // if(action.)
+
+    // next(action)
+};
 
 export default [ fetchFilmsMiddleware ];
